0d30f4fc292a9a10eeb347edccdc58aee5a690e7
Sugam Karki <sugamkarki7058@gmail.com>
2024-04-27 23:28:56 -0400

    about section done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
+import ParallaxTilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
+import { services } from "../constants";
+import SectionWrapper from "../hoc/SectionWrapper";
+const ServiceCard = ({ title, icon, index }) => {
+  return (
+    <ParallaxTilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img
+            src={icon}
+            alt="web-development"
+            className="w-16 h-16 object-contain"
+          />
 
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </ParallaxTilt>
+  );
+};
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview. </h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-2-3xl leading-[30px]"
+      >
+        Welcome! I'm Sugam Karki, a passionate web developer dedicated to
+        crafting unique and functional digital experiences. With years of
+        experience in the industry, I specialize in front-end development,
+        creating responsive websites that not only look great but also
+        prioritize usability and performance. My skills include HTML, CSS,
+        JavaScript, TypeScript and frameworks like React, NextJS, and Vue.js.
+        Whether it's building sleek user interfaces, optimizing code for
+        efficiency, or collaborating with teams to bring ideas to life, I thrive
+        on turning concepts into reality through clean, scalable code. Explore
+        my portfolio to see how I can elevate your digital presence.
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard
+            key={index}
+            index={index}
+            title={service.title}
+            icon={service.icon}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default About
+export default SectionWrapper(About, "about");
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 mb-4`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -14,7 +14,7 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915eff]">Sugam</span>
+            Hi, I'm <span className="text-[#915eff] mb-10">Sugam</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a bla bla developer and <br className="sm:block hidden" /> I
@@ -23,6 +23,21 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+      <div className="absolute xs:bottom-10 mt-3 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
